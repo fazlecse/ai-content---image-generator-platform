e95c0c065699815d2a4714baa6fb6f6421224a3f
Md. Fazle Rabbi <mdfazlerabbi06@gmail.com>
2023-04-01 16:41:31 +0600

    01-04-2023
    /user-panel-templates-blog-section done

--- assets/js/dashboard.js
+++ assets/js/dashboard.js
@@ -185,99 +185,7 @@ function preloder_function() {
     const useDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     const isSmallScreen = window.matchMedia('(max-width: 1023.5px)').matches;
 
-    tinymce.init({
-        selector: 'textarea.tinymce-editor',
-        plugins: 'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
-        editimage_cors_hosts: ['picsum.photos'],
-        menubar: 'file edit view insert format tools table help',
-        toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
-        toolbar_sticky: true,
-        toolbar_sticky_offset: isSmallScreen ? 102 : 108,
-        autosave_ask_before_unload: true,
-        autosave_interval: '30s',
-        autosave_prefix: '{path}{query}-{id}-',
-        autosave_restore_when_empty: false,
-        autosave_retention: '2m',
-        image_advtab: true,
-        link_list: [{
-            title: 'My page 1',
-            value: 'https://www.tiny.cloud'
-        },
-        {
-            title: 'My page 2',
-            value: 'http://www.moxiecode.com'
-        }
-        ],
-        image_list: [{
-            title: 'My page 1',
-            value: 'https://www.tiny.cloud'
-        },
-        {
-            title: 'My page 2',
-            value: 'http://www.moxiecode.com'
-        }
-        ],
-        image_class_list: [{
-            title: 'None',
-            value: ''
-        },
-        {
-            title: 'Some class',
-            value: 'class-name'
-        }
-        ],
-        importcss_append: true,
-        file_picker_callback: (callback, value, meta) => {
-            /* Provide file and text for the link dialog */
-            if (meta.filetype === 'file') {
-                callback('https://www.google.com/logos/google.jpg', {
-                    text: 'My text'
-                });
-            }
 
-            /* Provide image and alt text for the image dialog */
-            if (meta.filetype === 'image') {
-                callback('https://www.google.com/logos/google.jpg', {
-                    alt: 'My alt text'
-                });
-            }
-
-            /* Provide alternative source and posted for the media dialog */
-            if (meta.filetype === 'media') {
-                callback('movie.mp4', {
-                    source2: 'alt.ogg',
-                    poster: 'https://www.google.com/logos/google.jpg'
-                });
-            }
-        },
-        templates: [{
-            title: 'New Table',
-            description: 'creates a new table',
-            content: '<div class="mceTmpl"><table width="98%%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>'
-        },
-        {
-            title: 'Starting my story',
-            description: 'A cure for writers block',
-            content: 'Once upon a time...'
-        },
-        {
-            title: 'New list with dates',
-            description: 'New List with dates',
-            content: '<div class="mceTmpl"><span class="cdate">cdate</span><br><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>'
-        }
-        ],
-        template_cdate_format: '[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]',
-        template_mdate_format: '[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]',
-        height: 600,
-        image_caption: true,
-        quickbars_selection_toolbar: 'bold italic | quicklink h2 h3 blockquote quickimage quicktable',
-        noneditable_class: 'mceNonEditable',
-        toolbar_mode: 'sliding',
-        contextmenu: 'link image table',
-        skin: useDarkMode ? 'oxide-dark' : 'oxide',
-        content_css: useDarkMode ? 'dark' : 'default',
-        content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:16px }'
-    });
 
     /**
      * Initiate Bootstrap validation check
@@ -334,10 +242,55 @@ const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
 
 
-// select2 user panel blog section
-$(document).ready(function() {
-    $('.js-example-basic-single').select2({
-        maximumSelectionLength: 2
-    });
-
+// selcect2 template-view
+$(document).ready(function () {
+    $('.cmn-select2-dropdown').select2();
 });
+
+function formatState(state) {
+    if (!state.id) {
+        return state.text;
+    }
+    var baseUrl = "../images/mini-flag";
+    var $state = $(
+        '<span><img src="' + baseUrl + '/' + state.element.value.toLowerCase() + '.svg" class="img-flag" /> ' + state.text + '</span>'
+    );
+    return $state;
+};
+
+$(".js-example-basic-single").select2({
+    templateResult: formatState
+});
+
+// $(".js-example-basic-single").select2({
+//     templateResult: function (param) {
+//         var $span = $(
+//             "<span><img src='../images/mini-flag/" +
+//             param.id +
+//             "/1/tiny/" +
+//             param.id +
+//             ".png'/> " +
+//             param.text +
+//             "</span>" +
+//             "<p class='cname'>" +
+//             param.cname +
+//             "</p>"
+//         );
+//         return $span;
+//     },
+//     templateSelection: function (param) {
+//         var $span = $(
+//             "<span><img src='https://www.free-country-flags.com/countries/" +
+//             param.id +
+//             "/1/tiny/" +
+//             param.id +
+//             ".png'/> " +
+//             param.text +
+//             "</span>"
+//         );
+//         return $span;
+//     },
+//     dropdownCssClass: "bigdrop",
+// });
+
+
